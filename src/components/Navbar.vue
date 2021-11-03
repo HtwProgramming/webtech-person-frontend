61f84b957a2575a30fb180ebc80e31b3212a0893
Daniel Wagner <wagner.daniel87@gmail.com>
2021-11-03 19:28:05 +0100

    add bootstrap navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Das ist eine Navbar</h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">My Person App</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#">About</a>
+          <a class="nav-link" href="#">Persons</a>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
