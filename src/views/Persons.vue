fa39d2e4ce6627dcec4c9930171635941cb7ee9d
Daniel Wagner <wagner.daniel87@gmail.com>
2021-11-20 18:29:42 +0100

    add dynamic person view

--- src/views/Persons.vue
+++ src/views/Persons.vue
@@ -2,24 +2,14 @@
   <h1>Welcome to Persons</h1>
   <div class="container-fluid">
     <div class="row row-cols-1 row-cols-md-4 g-4">
-      <div class="col">
+      <div class="col" v-for="person in persons" :key="person.id">
         <div class="card h-100">
-          <img src="../assets/male.png" class="card-img-top" alt="Max Mustermann">
+          <img :src="getAvatar(person)" class="card-img-top" :alt="person.firstName + ' ' + person.lastName">
           <div class="card-body">
-            <h5 class="card-title">Max Mustermann</h5>
+            <h5 class="card-title">{{ person.firstName }} {{ person.lastName }}</h5>
             <p class="card-text">
-              Max Mustermann ist geimpft und hat keine Haustiere.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img src="../assets/female.png" class="card-img-top" alt="Maxima Meier">
-          <div class="card-body">
-            <h5 class="card-title">Maxima Meier</h5>
-            <p class="card-text">
-              Maxima Meier ist nicht geimpft und hat 1 Haustier.
+              {{ person.firstName }} {{ person.lastName }} ist {{ person.vaccinated ? 'geimpft' : 'nicht geimpft' }} und
+              hat {{ person.pets.length }} Haustier(e).
             </p>
           </div>
         </div>
@@ -30,7 +20,41 @@
 
 <script>
 export default {
-  name: 'Persons'
+  name: 'Persons',
+  data () {
+    return {
+      persons: [
+        {
+          id: 1,
+          firstName: 'Max',
+          lastName: 'Mustermann',
+          vaccinated: true,
+          gender: 'MALE',
+          pets: []
+        },
+        {
+          id: 2,
+          firstName: 'Maxima',
+          lastName: 'Meier',
+          vaccinated: false,
+          gender: 'FEMALE',
+          pets: [
+            1,
+            2
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+    getAvatar (person) {
+      if (person.gender === 'MALE') {
+        return require('../assets/male.png')
+      } else if (person.gender === 'FEMALE') {
+        return require('../assets/female.png')
+      }
+    }
+  }
 }
 </script>
 
