fce455fb693714a0bf0115af4d34ba8995cd7e4e
Daniel Wagner <wagner.daniel87@gmail.com>
2021-12-11 19:05:12 +0100

    create person form

--- src/components/PersonsCreateForm.vue
+++ src/components/PersonsCreateForm.vue
@@ -8,7 +8,37 @@
       <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
-      <p>The body</p>
+      <form class="needs-validation text-start" novalidate>
+        <div class="mb-3">
+          <label for="first-name" class="form-label">First name</label>
+          <input type="text" class="form-control" id="first-name" required>
+        </div>
+        <div class="mb-3">
+          <label for="last-name" class="form-label">Last name</label>
+          <input type="text" class="form-control" id="last-name" required>
+        </div>
+        <div class="mb-3">
+          <label for="gender" class="form-label">Gender</label>
+          <select id="gender" class="form-select">
+            <option value="" selected>Choose...</option>
+            <option value="MALE">Male</option>
+            <option value="FEMALE">Female</option>
+            <option value="DIVERSE">Diverse</option>
+          </select>
+        </div>
+        <div class="mb-3">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="vaccinated">
+            <label class="form-check-label" for="vaccinated">
+              Vaccinated
+            </label>
+          </div>
+        </div>
+        <div class="mt-5">
+          <button class="btn btn-primary me-3" type="submit">Create</button>
+          <button class="btn btn-danger" type="reset">Reset</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
